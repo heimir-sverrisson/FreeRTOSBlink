df3fd1e333df8e5ce70f97c151e0b987aaddafb7
Heimir Thor Sverrisson <heimir.sverrisson@gmail.com>
2023-03-22 18:50:45 -0600

    Attempt to set run target in FreeRTOS

--- source/sys_main.c
+++ source/sys_main.c
@@ -1,44 +1,9 @@
 /** @file sys_main.c 
-*   @brief Application main file
-*   @date 11-Dec-2018
-*   @version 04.07.01
-*
-*   This file contains an empty main function,
-*   which can be used for the application.
 */
 
 /* 
 * Copyright (C) 2009-2018 Texas Instruments Incorporated - www.ti.com 
 * 
-* 
-*  Redistribution and use in source and binary forms, with or without 
-*  modification, are permitted provided that the following conditions 
-*  are met:
-*
-*    Redistributions of source code must retain the above copyright 
-*    notice, this list of conditions and the following disclaimer.
-*
-*    Redistributions in binary form must reproduce the above copyright
-*    notice, this list of conditions and the following disclaimer in the 
-*    documentation and/or other materials provided with the   
-*    distribution.
-*
-*    Neither the name of Texas Instruments Incorporated nor the names of
-*    its contributors may be used to endorse or promote products derived
-*    from this software without specific prior written permission.
-*
-*  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS 
-*  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT 
-*  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
-*  A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT 
-*  OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, 
-*  SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT 
-*  LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
-*  DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
-*  THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT 
-*  (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE 
-*  OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-*
 */
 
 
@@ -57,6 +22,7 @@
 /* Include HET header file - types, definitions and function declarations for system driver */
 #include "het.h"
 #include "gio.h"
+#include "bl_run_target.h"
 
 /* Define Task Handles */
 xTaskHandle xTask1Handle;
@@ -80,45 +46,45 @@ int toggle(int value){
 void redTask(void *pvParameters)
 {
 	int red = 1;
+	int delay = 1000;
+	int target_counter = 10;
 
     for(;;){
     	set_value(PIN_RED, red);
     	red = toggle(red);
-    	vTaskDelay(1000);
+    	vTaskDelay(delay);
+    	if(--target_counter < 0){
+    		vTaskEndScheduler();
+//    		softReset();
+    	}
     }
 }
 
 void yellowTask(void *pvParameters)
 {
 	int yellow = 1;
+	int delay = 600;
 
     for(;;){
     	set_value(PIN_YELLOW, yellow);
     	yellow = toggle(yellow);
-    	vTaskDelay(600);
+    	vTaskDelay(delay);
     }
 }
 
 void greenTask(void *pvParameters)
 {
 	int green = 1;
+	int delay = 200;
 
     for(;;){
     	set_value(PIN_GREEN, green);
     	green = toggle(green);
-    	vTaskDelay(200);
+    	vTaskDelay(delay);
     }
 }
 
 /* USER CODE END */
-
-/** @fn void main(void)
-*   @brief Application main function
-*   @note This function is empty by default.
-*
-*   This function is called after startup.
-*   The user can use this function to implement the application.
-*/
 
 /* USER CODE BEGIN (2) */
 /* USER CODE END */
@@ -149,8 +115,8 @@ int main(void)
     /* Start Scheduler */
     vTaskStartScheduler();
 
-    /* Run forever */
-    while(1);
+	softReset();
+
 /* USER CODE END */
 
     return 0;
